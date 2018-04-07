5e9d3781c00082b3a4c58e28e9db3552d4517e11
robot <Zupken@spoko.pl>
2018-04-07 22:49:54 +0200

    Added func get_table_data.

--- main.py
+++ main.py
@@ -3,30 +3,50 @@ import lxml.html
 import scraper as sc
 
 
+
 class Scraping:
 
     def __init__(self):
         self.url = 'http://www.livescore.com/soccer/australia/a-league/'
         self.data = []
-
-    def get_data(self):
         # JSHandle has sometimes troubles with getting site source. Please restart the program, error should be gone.
-        source = sc.JSHandle(self.url)
-        tree = lxml.html.fromstring(source)
-        etree = tree.xpath('//div[@data-type="container"]')[0]
-        mount = int(tree.xpath('count(//div[@data-type="container"]/div[@data-type="evt"])'))
+        self.source = sc.JSHandle(self.url)
+        self.tree = lxml.html.fromstring(self.source)
+
+    def get_matches_data(self):
+        self.data = []
+        mount = int(self.tree.xpath('count(//div[@data-type="container"]/div[@data-type="evt"])'))
         for i in range(1, mount+1):
-            element = etree.xpath('.//div[@data-type="evt"]['+str(i)+']')[0]
+            element = self.tree.xpath('//div[@data-type="container"]//div[@data-type="evt"]['+str(i)+']')[0]
             date = sc.get_text(element.xpath('.//preceding::div[@class="right fs11"][1]/text()'))
             time = sc.get_text(element.xpath('.//div[@class="min "]/span/text()'))
             home_name = sc.get_text(element.xpath('.//div[@class="ply tright name"]/span/text()'))
             away_name = sc.get_text(element.xpath('.//div[@class="ply name"]/span/text()'))
             score_home = sc.get_text(element.xpath('.//span[@class="hom"]/text()'))
             score_away = sc.get_text(element.xpath('.//span[@class="awy"]/text()'))
-            print(date, time, home_name, score_home, away_name, score_away)
+            print(date, time, home_name, score_home, score_away, away_name)
             self.data.append([date, time, home_name, score_home, away_name, score_away])
-        sc.Database(('data', 'time', 'home_name', 'score_home', 'away_name', 'score_away')).database(self.data)
+        sc.Database(('data', 'time', 'home_name', 'score_home', 'away_name', 'score_away'), 'matches.db').database(self.data)
+
+    def get_table_data(self):
+        self.data = []
+        etree = self.tree.xpath('//div[@data-type="league-table-container"]/div[@data-type="table"]/div[@data-type="table-row"]')
+        for element in etree:
+            name = sc.get_text(element.xpath('./div[@class="team"]/text()'))
+            place = sc.get_text(element.xpath('.//span[@data-type="rank"]/text()'))
+            played = sc.get_text(element.xpath('.//div[@data-type="played"]/text()'))
+            wins = sc.get_text(element.xpath('.//div[@data-type="wins"]/text()'))
+            draws = sc.get_text(element.xpath('./div[@data-type="draws"]/text()'))
+            losses = sc.get_text(element.xpath('./div[@data-type="losses"]/text()'))
+            scored = sc.get_text(element.xpath('./div[@data-type="goals-scored"]/text()'))
+            received = sc.get_text(element.xpath('./div[@data-type="goals-received"]/text()'))
+            difference = sc.get_text(element.xpath('./div[@data-type="goal-difference"]/text()'))
+            points = sc.get_text(element.xpath('./div[@data-type="points"]/text()'))
+            print(name, place, played, wins, draws, losses, scored, received, difference, points)
+            self.data.append([name, place, played, wins, draws, losses, scored, received, difference, points])
+        sc.Database(('name', 'place', 'played', 'wins', 'draws', 'losses', 'scored', 'received', 'difference', 'points'), 'table.db').database(self.data)
 
 
 Scraping = Scraping()
-Scraping.get_data()
+Scraping.get_table_data()
+Scraping.get_matches_data()
